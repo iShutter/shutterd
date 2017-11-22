0ed9df439922f11960b8e453f3e3668e88d24c95
xD4rk1337 <martin.stundner@live.de>
2017-11-22 15:33:22 +0100

    init

---
+++ client.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys import paho.mqtt.client as mqtt import RPi.GPIO as GPIO 
+import time GPIO.setwarnings(False) GPIO.setmode(GPIO.BCM) GPIO.setup(4, 
+GPIO.OUT) GPIO.setup(17, GPIO.OUT) GPIO.setup(27, GPIO.OUT) 
+GPIO.output(4, 0) GPIO.output(17, 0) GPIO.output(27, 0) class DevNull:
+    def write(self, msg):
+        pass sys.stderr = DevNull()
+# The callback for when the client receives a CONNACK response from the 
+# server.
+def on_connect(
+    client,
+    userdata,
+    flags,
+    rc,
+    ):
+    time.sleep(3)
+    print("Connected with result code " + str(rc))
+    GPIO.output(27, 1)
+    time.sleep(3)
+    # Subscribing in on_connect() means that if we lose the connection 
+    # and reconnect then subscriptions will be renewed.
+    client.subscribe("test")
+    print("Subscribed to topic with result code " + str(rc))
+    GPIO.output(17, 1)
+    print("Ready for receiving messages")
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    message = str(msg.payload)
+    msgs = message.split("'")[1]
+    # set up pin 17 GPIO.setup(18, GPIO.OUT) # set up pin 18 
+    # GPIO.output(4, 1) # turn on pin 17 GPIO.output(18, 1) # turn on 
+    # pin 18 print(str(msg.payload))
+    if msgs == "on":
+        print("Trigger message received. Status: on")
+        GPIO.output(4,1)
+    else:
+        print("Trigger message received. Status: off")
+        GPIO.output(4,0) client = mqtt.Client() client.on_connect = 
+on_connect client.on_message = on_message client.connect("10.77.96.202", 
+1883)
+# Blocking call that processes network traffic, dispatches callbacks and 
+# handles reconnecting. Other loop*() functions are available that give 
+# a threaded interface and a manual interface.
+client.loop_forever()
